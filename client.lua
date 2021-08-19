bd3520964e1fc4f9de1bf56c67bfd76ee8a5ff6f
HarveyStreet <65784719+HarveyStreet@users.noreply.github.com>
2021-08-19 15:23:33 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,23 @@
+-- Made by Harvey S.
+-- =============================================================
+-- |  Only mess around in here if you know what you are doing! |
+-- =============================================================
+local rag = false
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if rag then
+            SetPedToRagdoll(GetPlayerPed(-1), Config.GetupSpeed, Config.GetupSpeed, Config.ragtype, false, false, false)
+        end
+    end
+end)
+
+RegisterCommand('+rag', function()
+    rag = true
+end, false)
+
+RegisterCommand('-rag', function()
+    rag = false
+end, false)
+
+RegisterKeyMapping('+rag', 'Ragdoll', 'keyboard', 'F9')
