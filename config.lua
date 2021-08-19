0fa4634a19bc8ad6ffd0087029f4839e1d7624b0
HarveyStreet <65784719+HarveyStreet@users.noreply.github.com>
2021-08-19 15:22:05 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+Config.GetupSpeed = 2000 -- The speed you get up
+
+Config.ragtype = 0
+-- 0 - Normal Ragdoll
+-- 1 - Fall with stiff legs/body
+-- 2 - stumble
+-- 3 - wide-leg stuble
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+-- Do not remove
+author 'Harvey S.'
+
+client_scripts {
+    'client/client.lua',
+    'config.lua'
+}
